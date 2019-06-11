b14d39a000f1b23a265a4b4741ce20f4bb15fb03
kiranraju <kiranraju.jakka@gmail.com>
2019-06-11 23:46:35 +0530

    added props & events

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,34 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <HelloWorld :msg="message" @messageChanged="message = $event"/>
+    <p>{{message}}</p>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
     HelloWorld
+  },
+  props: {},
+  data() {
+    return {
+      message: "Welcome to Your Vue.js App"
+    };
+  },
+  methods: {
+    changed() {}
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
